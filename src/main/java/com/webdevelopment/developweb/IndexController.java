d4f2ae50ccb58bfd4b48fae0d1186f8e41d37b23
markl <markl@L146.ehi.ehealth.com>
2016-07-04 15:47:27 +0800

    new

--- src/main/java/com/webdevelopment/developweb/IndexController.java
+++ src/main/java/com/webdevelopment/developweb/IndexController.java
@@ -1,5 +1,8 @@
 package com.webdevelopment.developweb;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class IndexController {
 
+	
 	@RequestMapping("/login")
 	public String login() {
 	
@@ -19,14 +23,20 @@ public class IndexController {
 	}
 	
 	@RequestMapping(value="/login",method = RequestMethod.POST)
-	public String create(@RequestParam("username") String username,
-			             @RequestParam("password") String password,Model model) {
+	public String index(@RequestParam("username")  String username,
+			             @RequestParam("password") String password,Model model,
+			             HttpServletRequest request) {
+		
+		HttpSession session = request.getSession();
+
 		model.addAttribute("username",username);
 		model.addAttribute("posts", Data.posts);
+		
 		for(Users user :Data.users) {
 			if(username.equals(user.getUsername())) {
 				if(password.equals(user.getPassword())) {
-					return "index";
+					session.setAttribute("username", username);
+					return "redirect:/index";
 				}else {
 					model.addAttribute("Error", "Password");
 					return "error";
@@ -38,6 +48,39 @@ public class IndexController {
 		}
 		return "login";
 	}
+	
+	@RequestMapping("/create")
+	public String create(HttpSession session) {
+		
+		if(session.getAttribute("username") == null) {
+			return "redirect:/login";
+		}
+		
+		return "create";
+	}
+	
+	@RequestMapping(value="/create",method = RequestMethod.POST)
+	public String backindex(@RequestParam("title") String title,
+							@RequestParam("content") String content, Model model,
+							HttpSession session) {
+		Data.posts.add(new Post(title,content));
+		model.addAttribute("posts", Data.posts);
+		model.addAttribute("username",session.getAttribute("username"));
+			
+		return "redirect:/index";
+	}
+	
+	@RequestMapping(value="/index")
+	public String goindex(Model model,HttpSession session) {
+		model.addAttribute("username",session.getAttribute("username"));
+		model.addAttribute("posts",Data.posts);
+		
+		if(session.getAttribute("username") == null) {
+			return "redirect:/login";
+		}
+		return "index";
+	}
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(IndexController.class, args);
